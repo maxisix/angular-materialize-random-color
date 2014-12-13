47e70f67d422f5b2803e1fafd866b3e499a412cd
SharkEleven <dams.shark@gmail.com>
2014-12-13 16:15:03 -0500

    add UserFactory and prepare app to save user preference

--- app/app.module.js
+++ app/app.module.js
@@ -4,7 +4,7 @@ require('angular');
 
 var app = angular.module('MaterialColor', []);
 
-	app.factory('ActionFactory', function() {
+	app.factory('ColorFactory', function() {
 
 		var factory = {
 			colors : [
@@ -28,21 +28,25 @@ var app = angular.module('MaterialColor', []);
 				"grey",
 				"blue-grey"
 			],
-			getColor: function() {
-				// here function to get random colors
+			getRandomColor: function() {
+				// function to get random colors
+				var news = new Array;
+				
 				random1 = Math.floor((Math.random() * 19)),
 				random2 = Math.floor((Math.random() * 19));
 
-				var random_primary = document.getElementById('random-primary');
-				var random_secondary = document.getElementById('random-secondary');
+				// var random_primary = document.getElementById('random-primary');
+				// var random_secondary = document.getElementById('random-secondary');
 
-				random_primary.classList.add(factory.colors[random1]);
-				random_secondary.classList.add(factory.colors[random2]);
+				news['primary'] = factory.colors[random1];
+				news['secondary'] = factory.colors[random2];
+				// random_primary.classList.add(factory.colors[random1]);
+				// random_secondary.classList.add(factory.colors[random2]);
 
-				return false;
+				return news;
 			},
-			saveColor: function(id) {
-				// here function to copy hex in clipboard 
+			copyColor: function(id) {
+				// function to copy color in clipboard
 				return false;
 			}
 
@@ -51,6 +55,54 @@ var app = angular.module('MaterialColor', []);
 		return factory;
 	} );
 
-	app.controller( 'ColorsCtrl', function($scope, ActionFactory) {
-		$scope.colors = ActionFactory.getColor();
+	app.factory('UserFactory', function() {
+
+		var factory = {
+			colors : [
+				{
+					"id": 0,
+					"primary": "blue",
+					"secondary": "red"
+				},
+				{
+					"id": 1,
+					"primary": "purple",
+					"secondary": "lime"
+				},
+				{
+					"id": 2,
+					"primary": "blue",
+					"secondary": "brown"
+				}
+			],
+			getColorSaved: function() {
+				// return color saved by user 
+				return factory.colors;
+			},
+			saveColor: function(news) {
+				// function to save colors in user preferences
+				return false;
+			},
+			removeColor: function(id) {
+				// function to remove colors from user preferences
+				return false;
+			}
+
+		}
+
+		return factory;
+	} );
+
+	app.controller( 'ColorsCtrl', function($scope, ColorFactory, UserFactory) {
+		$scope.colors = UserFactory.getColorSaved();
+
+		$scope.save = function() {
+			//save colors in user preferences
+			UserFactory.saveColor(news);
+		}
+
+		$scope.random = function() {
+			var news = ColorFactory.getRandomColor();
+			console.log( news );
+		}
 	} );
