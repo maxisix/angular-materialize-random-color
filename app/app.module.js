95e23a3e3588c0df6de9d495e81aa4b277adb9a4
SharkEleven <dams.shark@gmail.com>
2014-12-13 13:26:49 -0500

    move simple script into angular service

--- app/app.module.js
+++ app/app.module.js
@@ -7,8 +7,38 @@ var app = angular.module('MaterialColor', []);
 	app.factory('ActionFactory', function() {
 
 		var factory = {
+			colors : [
+				"red",
+				"pink",
+				"purple",
+				"deep-purple",
+				"indigo",
+				"blue",
+				"light-blue",
+				"cyan",
+				"teal",
+				"green",
+				"light-green",
+				"lime",
+				"yellow",
+				"amber",
+				"orange",
+				"deep-orange",
+				"brown",
+				"grey",
+				"blue-grey"
+			],
 			getColor: function() {
-				// here function to get random color
+				// here function to get random colors
+				random1 = Math.floor((Math.random() * 19)),
+				random2 = Math.floor((Math.random() * 19));
+
+				var random_primary = document.getElementById('random-primary');
+				var random_secondary = document.getElementById('random-secondary');
+
+				random_primary.classList.add(factory.colors[random1]);
+				random_secondary.classList.add(factory.colors[random2]);
+
 				return false;
 			},
 			saveColor: function(id) {
