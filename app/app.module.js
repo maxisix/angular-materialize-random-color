1874a7831fc00ba8e0c801b80bdf7830986e2bf8
SharkEleven <dams.shark@gmail.com>
2014-12-13 11:22:40 -0500

    add first angular structure (with factory)

--- app/app.module.js
+++ app/app.module.js
@@ -1,3 +1,26 @@
 'use strict';
 
 require('angular');
+
+var app = angular.module('MaterialColor', []);
+
+	app.factory('ActionFactory', function() {
+
+		var factory = {
+			getColor: function() {
+				// here function to get random color
+				return false;
+			},
+			saveColor: function(id) {
+				// here function to copy hex in clipboard 
+				return false;
+			}
+
+		}
+
+		return factory;
+	} );
+
+	app.controller( 'ColorsCtrl', function($scope, ActionFactory) {
+		$scope.colors = ActionFactory.getColor();
+	} );
